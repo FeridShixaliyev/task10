1dd0586070b9423ffc4ab49e40787b76bbdb8b11
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-29 11:58:04 +0400

    task 2

---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp33
+{
+    class Student
+    {
+        public string _fullname;
+        public string _groupno;
+        public int Age { get; set; }
+
+        public string GroupN0
+        {
+            get => _groupno;
+            set
+            {
+                if (_groupno.Length == 5 && char.IsUpper(_groupno[0]) && char.IsUpper(_groupno[1]) && char.IsDigit(_groupno[2]) && char.IsDigit(_groupno[3]) && char.IsDigit(_groupno[4]))
+                    this._groupno = value;
+            }
+        }
+
+        string Name;
+        string Surname;
+        public string FullName
+        {
+            get => _fullname;
+            set
+            {
+                if (value==Name+" "+Surname)
+                    _fullname = value;
+            }
+        }
+
+        public static bool CheckGroupNo()
+        {
+            if()
+        }
+        public static bool CheckFullname()
+        {
+            
+        }
+
+    }
+}
